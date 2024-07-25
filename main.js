d440399d3d8d30e1022f6361c199062390129abf
Pratik Chapagain <chapagainpratik28@gmail.com>
2024-07-25 17:28:33 +0545

    push

---
+++ main.js
@@ -0,0 +1,13 @@
+//get the canvas element and its 2d drawing context
+const canvas = document.getElementById('pongCanvas');
+const ctx = canvas.getContext('2d');
+
+//game settings
+const paddlewidth = 10;
+const paddleheight = 100;
+const ballradius = 10;
+const ballAcceleration = 0.1;//it determines the speed of the ball//
+const paddleSpeed = 5;
+const winningScore = 5;
+
+alert("you won");
